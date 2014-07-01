28ffbccc835c8a7607b714ac1f4a40eb39f30146
Matt Kilgore <mattkilgore12@gmail.com>
2014-07-01 16:28:56 -0400

    Change dump_mem call to skip identical lines

--- src/common/common.c
+++ src/common/common.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
 
@@ -53,26 +54,58 @@ int stringcasecmp(const char *s1, const char *s2)
 
 void dump_mem(void *buf, size_t len, uint32_t base_addr)
 {
+    char strbuf[100], strbuf2[100];
+    char *cur_b, *start, *to_print;
     unsigned char *b = buf;
-    int i = 0, j;
+    int i = 0, j, skipping = 0;
+
+    cur_b = strbuf;
+    start = strbuf;
+
     for (; i < len; i += 16) {
-        printf("0x%08x  ", (i) + base_addr);
+        cur_b += sprintf(cur_b, "0x%08x  ", (i) + base_addr);
         for (j = i; j < i + 16; j++) {
             if (j < len)
-                printf("%02x ", (unsigned char)b[j]);
+                cur_b += sprintf(cur_b, "%02x ", (unsigned char)b[j]);
             else
-                printf("   ");
+                cur_b += sprintf(cur_b, "   ");
             if (j - i == 7)
-                putchar(' ');
+                *(cur_b++) = ' ';
         }
 
-        printf(" |");
+        cur_b += sprintf(cur_b, " |");
         for (j = i; j < i + 16 && j < len; j++)
             if (b[j] > 31 && b[j] <= 127)
-                printf("%c", b[j]);
+                cur_b += sprintf(cur_b, "%c", b[j]);
             else
-                putchar('.');
-        printf("|\n");
+                *(cur_b++) = '.';
+        cur_b += sprintf(cur_b, "|\n");
+
+        to_print = start;
+
+        if (start == strbuf)
+            start = strbuf2;
+        else
+            start = strbuf;
+
+        cur_b = start;
+
+        /* The 12 magic number is just so we don't compare the printed address,
+         * which is in '0x00000000' format at the beginning of the string */
+        if (strcmp(strbuf + 12, strbuf2 + 12) != 0) {
+            if (skipping == 1)
+                printf("%s", start);
+            else if (skipping == 2)
+                printf("...\n");
+            skipping = 0;
+            printf("%s", to_print);
+        } else if (skipping >= 1) {
+            skipping = 2;
+        } else {
+            skipping = 1;
+        }
     }
+    if (skipping)
+        printf("...\n%s", to_print);
 }
 
